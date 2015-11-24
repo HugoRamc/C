73e310e990386cc5bba7fef69dc7cfa534933315
kitkezo <tevod@hotmail.com>
2015-11-23 22:42:26 -0600

    llenar tablero completo :D

--- main.c
+++ main.c
@@ -1,35 +1,234 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void tiradas();
-int validarTablero(int);
-
+void tiradas(char [3][3],int);
+int validarTablero(char [3][3]);
+int validarFilas(char [3][3]);
+int validarColumnas(char [3][3]);
+int validarDiagonales(char [3][3]);
 
 int main(int argc, char *argv[])
 {
-	char tablero[3];
-	int cont = validarTablero(tablero[3]);
+	int i,j,contv;
+	char gato[3][3] = {'a','a','a','a','a','a','a','a','a'};
+	int cont = 2;
+	int turno = 2;
 	
-	while(cont != 1 && cont != 0)//Ganador con 0 las o ganador con 1 las x
+	while(cont != 0)//Ganador con 0 las o ganador con 1 las x
 	{
+		//cont = validarTablero(gato);
+		/*if(cont == 1)
+		{
+			break;
+		}*/
 		
+		if(turno == 2)
+		{
+			turno =1;
+		}else
+		{
+			if(turno == 1)
+			{
+				turno =2;
+			}
+		}
+		tiradas(gato,turno);
+		
+		for(i=0;i<3;++i)
+		{
+			for(j=0;j<3;++j)
+			{
+				if(gato[i][j]=='a')
+				{
+					contv++;
+				}
+			}
+		}
+		if(contv ==0)
+		{
+			printf("Nadie Ganó, :v");
+			//break;
+		}
 		
 		
 	}
-	
-	
+
+	printf("Gracias por jugar gato :D");	
 	return 0;
 }
 
-void tiradas()
+void tiradas(char tablero[3][3],int turno)
 {
+	int i,j,ip,jp;
+	
+	printf("   0    1   2   ");
+	printf("\n"); 
+	for(i=0;i<3;++i)//imprime el tablero
+	{
+		printf("%d  ",i);
+		
+		for(j=0;j<3;++j)
+		{
+			
+			if(tablero[i][j] == 'a')
+			{
+				printf("  |  ");
+			}else
+			{
+				if(tablero[i][j] == 'X' || tablero[i][j] == 'x')
+				{
+					printf(" X  |");
+				}else
+				{
+					if(tablero[i][j] == 'O' || tablero[i][j] == 'o')
+					{
+						printf(" O  |");
+					}
+				}
+			}
+		}
+		printf("\n");
+		
+	}
+	
+	printf("\n");
+	
+	printf("Seleccione las coordenadas de la casilla a tirar\n");
+	printf("fila: ");
+	scanf("%d",&ip);
+	printf("\n Columna: ");
+	scanf("%d",&jp);
+	
+	if(tablero[ip][jp]!='X' && tablero[ip][jp]!='=')
+	{
+		if(turno == 1)
+		{
+			tablero[ip][jp] = 'X';
+		}else
+		{
+			if(turno == 2)
+			{
+				tablero[ip][jp] = 'O';
+			}
+		}
+	}else
+	{
+		printf("\n Casilla ocupada: Vuelve a Tirar");
+		printf("\n\n\n");
+		tiradas(tablero,turno);
+	}
 	
 }
 
-int validarTablero(char tablero[3])
+
+int validarTablero(char tablero[3][3])
 {
+	int i=0,j=0, ganador;
+
+	//ganador = validarFilas(tablero[3][3]);
+	/*if(ganador == 1)
+	{
+		return ganador;
+	}else
+	{
+		ganador = validarColumnas(tablero);
+		if(ganador == 1)
+		{
+			return ganador;
+		}else
+		{
+			ganador = validarDiagonales(tablero);
+			
+		}
+	}*/
+	
+	return ganador;
+}
+
+int validarFilas(char tablero[3][3])
+{
+	int gan =0,i,j;
+	for(i=0;i<3;i++)
+	{
 		
+		if((tablero[i][j] == "O" || tablero[i][j] == "o")&& (tablero[i][j+1] == "O" || tablero[i][j+1] == "o") && (tablero[i][j+2] == "O" || tablero[i][j+2] == "o"))
+		{
+			printf("El ganador es el jugador 1");
+			gan = 1;
+			break;
+		}else
+		{
+			if((tablero[i][j] == "X" || tablero[i][j] == "x")&& (tablero[i][j+1] == "X" || tablero[i][j+1] == "x") && (tablero[i][j+2] == "X" || tablero[i][j+2] == "x"))
+			{
+				printf("El ganador es el jugador 2");
+				gan = 1;
+				break;
+			}
+		}
+		j=0;
+	}
+	return gan;
+}
 
+int validarColumnas(char tablero[3][3])
+{
+	int gan =0,i,j;
+	for(i=0,j=0;j<3;j++)
+	{
+		
+		if((tablero[i][j] == "O" || tablero[i][j] == "o")&& (tablero[i+1][j] == "O" || tablero[i+1][j] == "o") && (tablero[i+2][j] == "O" || tablero[i+2][j] == "o"))
+		{
+			printf("El ganador es el jugador 1");
+			gan = 1;
+			break;
+		}else
+		{
+			if((tablero[i][j] == "X" || tablero[i][j] == "x")&& (tablero[i+1][j] == "X" || tablero[i+1][j] == "x") && (tablero[i+2][j] == "X" || tablero[i+2][j] == "x"))
+			{
+				printf("El ganador es el jugador 2");
+				gan = 1;
+				break;
+			}
+		}
+		i=0;
+	}
+}
 
-
+int validarDiagonales(char tablero[3][3])
+{
+		int gan =0,i,j;
+	
+		
+		if((tablero[0][0] == "O" || tablero[0][0] == "o")&& (tablero[1][1] == "O" || tablero[1][1] == "o") && (tablero[2][2] == "O" || tablero[2][2] == "o"))
+		{
+			printf("El ganador es el jugador 1");
+			gan = 1;
+			
+		}else
+		{
+			if((tablero[0][2] == "O" || tablero[0][2] == "o")&& (tablero[1][1] == "O" || tablero[1][1] == "o") && (tablero[2][0] == "O" || tablero[2][0] == "o"))
+			{
+				printf("El ganador es el jugador 1");
+				gan = 1;
+				
+			}else
+			{
+					if((tablero[0][0] == "X" || tablero[0][0] == "x")&& (tablero[1][1] == "X" || tablero[1][1]== "x") && (tablero[2][2] == "X" || tablero[2][2] == "x"))
+					{
+						printf("El ganador es el jugador 2");
+						gan = 1;
+						
+					}else
+					{
+						if((tablero[0][2] == "X" || tablero[0][2] == "x")&& (tablero[1][1] == "X" || tablero[1][1] == "x") && (tablero[2][0] == "X" || tablero[2][0] == "o"))
+						{
+							printf("El ganador es el jugador 2");
+							gan = 1;
+							
+						}
+					}
+			}
+		}
+		
+	return gan;
 }
