c0b7b9c78ea202a5f88a5f66fe4056ee7c9c31ca
kitkezo <tevod@hotmail.com>
2015-11-23 21:09:25 -0600

    Añadido Validaciones del tablero; faltan tiradas

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tiradas();
+int validarTablero(int);
+
+
+int main(int argc, char *argv[])
+{
+	char tablero[3];
+	int cont = validarTablero(tablero[3]);
+	
+	while(cont != 1 && cont != 0)//Ganador con 0 las o ganador con 1 las x
+	{
+		
+		
+		
+	}
+	
+	
+	return 0;
+}
+
+void tiradas()
+{
+	
+}
+
+int validarTablero(char tablero[3])
+{
+		
+
+
+
+}
