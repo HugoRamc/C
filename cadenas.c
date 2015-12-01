da850a543a223c33bef905d4a123e3336f3b5dd8
HRC <HRC@mail.com>
2015-12-01 09:54:53 -0600

    Agregado ejercicio comparacion de cadenas

--- cadenas.c
+++ cadenas.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void validaCaracter(char);
 
 int main(int argc, char *argv[])
 {
 	int i=0;
-	char cad[20];
+	char cad[20],cad2[20];
 	printf("Ingresa una frase: ");
 	gets(cad);
 	printf("\n");
+	printf("Ingresa otra frase: ");
+	gets(cad2);
 	
-	while(cad[i] != NULL)
+	if(strcmp(cad,cad2)==0)
+	{
+		printf("%s es igual a %s",cad,cad2);
+	}else
+	{
+		printf("%s es diferente a %s",cad,cad2);
+	}
+	
+	/*while(cad[i] != NULL)
 	{
 		validaCaracter(cad[i]);
 		++i;
-	}
+	}*/
 	
 	
 	printf("\n");
